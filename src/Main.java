5a8e797beee59d3be5b5ea9084837c4fcdce42a8
daniilusacev <2893318@gmail.com>
2023-05-14 18:56:57 +0300

    task2 done

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,17 @@ public class Main {
     public static void main(String[] args) {
         /* task1 */
         int age = 15;
-        if (age >= 18){
+        if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
-        }
-        else {
+        } else {
             System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+        /* task2 */
+        int temperature = 7;
+        if (temperature <= 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
         }
     }
 }
